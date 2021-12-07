e39acca5ce6763af2eec2c6d853792c87e167b36
In69 <90973339+In69@users.noreply.github.com>
2021-12-07 09:19:48 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+console.log("Hello Winc Academy-studenten")
+
+// Print "Dit is een grote som!" to the console
+console.log("Dit is een grote som!");
+
+
+let p = 1230941*1823794;
+console.log(p)
+
+
+let h = 637263/ 54;
+console.log(h)
+
+// Print "1230941*1823794 and 637263/ 54 " to the console
+console.log("1230941*1823794 and 637263/ 54 ");
